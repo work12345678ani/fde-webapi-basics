5ed1139f1a5d7d3d5ae67c32f250c9a8704867d8
Anirudh Ganapathy P S <anirudh.pg@prodapt.com>
2025-11-17 14:33:02 +0530

    bleh

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+  return {"hello": "world"}
 
 @app.get("/health")
 async def health():
