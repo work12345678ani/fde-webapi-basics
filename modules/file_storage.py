00e0e9ec5b65a17b88669e6c936a32d882a1edf4
Anirudh Ganapathy P S <anirudh.pg@prodapt.com>
2025-11-25 12:16:14 +0530

    random file names go brr

--- modules/file_storage.py
+++ modules/file_storage.py
@@ -1,4 +1,6 @@
 import os
+import mimetypes
+import uuid
 from supabase import create_client, Client
 from .config import settings
 
@@ -8,11 +10,11 @@ UPLOAD_DIR="uploads"
 def upload_file(bucket_name, path, contents, content_type):
     if settings.PRODUCTION:
         response = supabase.storage.from_(bucket_name) \
-                    .upload(path, contents, {"content-type": content_type, "upsert": "true"})
+                    .upload(str(uuid.uuid4()) + mimetypes.guess_extension(content_type), contents, {"content-type": content_type})
         return f"{str(settings.SUPABASE_URL)}/storage/v1/object/public/{response.full_path}"
     else:
         os.makedirs(UPLOAD_DIR, exist_ok=True)
         file_path = os.path.join(UPLOAD_DIR, bucket_name, path)
         with open(file_path, "wb") as f:
             f.write(contents)
-        return f"/{UPLOAD_DIR}/{bucket_name}/{path}"
+        return f"/{UPLOAD_DIR}/{bucket_name}/{str(uuid.uuid4()) + mimetypes.guess_extension(content_type)}"
